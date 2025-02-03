96ab39f77ed6f9200dce69bcd101825ac1555da0
Axel Adlercreutz <39679469+DoebelnAtz@users.noreply.github.com>
2025-02-03 13:00:40 +0200

    accidentally made the hook a function call rather than having it return a fumnction... (#41)

--- gradesa/src/shared/hooks/useRequest.js
+++ gradesa/src/shared/hooks/useRequest.js
@@ -7,16 +7,16 @@ const defaultConfig = {
   },
 };
 
-/**
- * @description This hook is used to make a request to the server.
- * Mainly used for non-GET requests.
- * @param {string} url - The URL to make the request to.
- * @param {Object} body - The body of the request.
- * @param {Object} config - The config of the request.
- * @returns {Object} - The response from the server.
- */
-export const useRequest = (url, body, config) => {
-  return axiosInstance.request({
+export const useRequest = () => {
+  /**
+   * @description This hook is used to make a request to the server.
+   * Mainly used for non-GET requests.
+   * @param {string} url - The URL to make the request to.
+   * @param {Object} body - The body of the request.
+   * @param {Object} config - The config of the request.
+   * @returns {Object} - The response from the server.
+   */
+  return (url, body, config) => axiosInstance.request({
     url,
     data: body,
     ...defaultConfig,
